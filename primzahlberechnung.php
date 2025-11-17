11c074e211b4491c5d2f703360d75f8acfcb1551
baumkircher1 <152158412+baumkircher1@users.noreply.github.com>
2025-11-17 15:19:04 +0100

    Primzahlenberechnung und Logarithmus Aufgaben

---
+++ primzahlberechnung.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Primzahlen und Logarithmus</title>
+</head>
+<body>
+<h2>Aufgabe 1: Primzahlen</h2>
+<!-- Formular für Zahleneingabe -->
+<form method="post">
+    <input type="number" name="zahl" value="<?php echo isset($_POST['zahl']) ? $_POST['zahl'] : 50; ?>">
+    <button type="submit" name="berechne_prim">Berechnen</button>
+</form>
+
+<?php
+// Prüfen ob Button geklickt wurde
+if (isset($_POST['berechne_prim'])) {
+    $n = $_POST['zahl'];
+    echo "<p>Primzahlen von 1 bis $n:</p>";
+
+    // Alle Zahlen von 2 bis n durchgehen
+    for ($num = 2; $num <= $n; $num++) {
+        $istPrim = true;
+
+        // Zahl durch alle Zahlen von 2 bis n/2 teilen
+        for ($i = 2; $i <= $num / 2; $i++) {
+            // Wenn teilbar, dann keine Primzahl
+            if ($num % $i == 0) {
+                $istPrim = false;
+                break;
+            }
+        }
+
+        // Wenn Primzahl, dann ausgeben
+        if ($istPrim) {
+            echo $num . "<br>";
+        }
+    }
+}
+?>
+
+<hr>
+
+<h2>Aufgabe 2: Logarithmus</h2>
+<form method="post">
+    <button type="submit" name="berechne_log">Berechnen</button>
+</form>
+
+<?php
+// Prüfen ob Button geklickt wurde
+if (isset($_POST['berechne_log'])) {
+    echo "<pre>";
+
+    // Für jede Zahl von 1 bis 100
+    for ($n = 1; $n <= 100; $n++) {
+        // Logarithmus berechnen
+        $log = log($n);
+
+        // Anzahl Sterne = gerundeter Log-Wert
+        $anzahlSterne = round($log);
+
+        // Sterne erstellen
+        $sterne = str_repeat('*', $anzahlSterne);
+
+        // Ausgabe: Zahl + Sterne
+        echo "$n:   $sterne\n";
+    }
+
+    echo "</pre>";
+}
+?>
+</body>
+</html>
